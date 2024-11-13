36b1d6ca89d0f05a73a1818c5e706ef3e8042800
belokonovaa <belokonovaa@yandex.ru>
2024-11-13 17:55:40 +0300

    готовы страницы - доставка и контакты

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from main.views import index, about
+from main.views import index, about, delivery, information
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('delivery/', delivery, name='delivery'),
+    path('information/', information, name='info'),
 ]
