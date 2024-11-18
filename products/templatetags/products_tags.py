fa28f96fb1c761699e9341b80bd7e236f99fd25b
belokonovaa <belokonovaa@yandex.ru>
2024-11-18 12:05:28 +0300

    Добавлена фильтрация товаров

--- products/templatetags/products_tags.py
+++ products/templatetags/products_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 
 from products.models import Category
 
@@ -8,3 +9,9 @@ register = template.Library()
 @register.simple_tag()
 def tag_category():
     return Category.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
