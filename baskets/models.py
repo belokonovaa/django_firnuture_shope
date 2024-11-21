f70f7a2948431ed63e7178f340e279699e3b3e3d
belokonovaa <belokonovaa@yandex.ru>
2024-11-21 12:48:59 +0300

    Добавлена возможность добавлять товары в корзину

--- baskets/models.py
+++ baskets/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from products.models import Product
+
+
+class CartQueryset(models.QuerySet):
+
+    def total_price(self):
+        return sum(basket.products_price() for basket in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(basket.quantity for basket in self)
+        return 0
+
+
+class Basket(models.Model):
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь')
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    session_key = models.CharField(max_length=32, blank=True, null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        db_table = 'baskets'
+        verbose_name = 'корзину'
+        verbose_name_plural = 'корзины'
+
+    objects = CartQueryset().as_manager()
+
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f'Корзина пользователя {self.user.username} | Товар: {self.product.name} | Количество: {self.quantity}'
--- baskets/views.py
+++ baskets/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from baskets.models import Basket
+from products.models import Product
 
 
-def basket_add(request, product_id):
+def basket_add(request, product_slug):
+
+    product = Product.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        baskets = Basket.objects.filter(user=request.user, product=product)
+
+        if baskets.exists():
+            basket = baskets.first()
+            if basket:
+                basket.quantity += 1
+                basket.save()
+        else:
+            Basket.objects.create(user=request.user, product=product, quantity=1)
+
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def basket_change(request, product_slug):
     ...
 
 
-def basket_change(request, product_id):
-    ...
-
-
-def basket_delete(request, product_id):
+def basket_delete(request, product_slug):
     ...
